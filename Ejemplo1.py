d606230d654b6e5883d10f50bb96d02dc7424eb3
yato515 <humberto.colli555@gmail.com>
2024-02-18 02:58:07 -0600

    boton accion

--- Ejemplo1.py
+++ Ejemplo1.py
@@ -1,42 +1,67 @@
 import tkinter as tk
 
-#colores
+# colores
 fond_botonE = "#38B6FF"
 fondo_entrada = "#B5B2B2"
 fondo_ventana = "#9FFCFD"
 fondo_frame = "#000000"
 
-#Ventana
+# Ventana
 
 ventana = tk.Tk()
-frame =tk.Frame()
+frame = tk.Frame()
 ventana.title("Pantalla 1")
-ventana.geometry("385x160+400+50") #el 450 y el 50 es para saber la pocision de la ventana
-#ventana.resizable(width=False, height=False)
-ventana.config( bg= fondo_ventana )
-ventana.iconbitmap("dragon.ico") #cambiamos el icono 
+ventana.geometry("385x160+400+50")
+ventana.config(bg=fondo_ventana)
+ventana.iconbitmap("dragon.ico")
 
+# Métodos
+entrada = tk.StringVar()
+entrada2 = tk.StringVar()
 
-#Texto
+def correcta():
+    ventana.withdraw()
+    ventana1 = tk.Toplevel()
+    ventana1.title("Ingresaste")
+    ventana1.geometry("400x400+450+50")
+    ventana1.config(bg=fondo_ventana)
+    ventana1.iconbitmap("dragon.ico")
 
-texto = tk.Label(ventana, text="Nombre",font=("Book Antiqua",12,"bold"), bg= fondo_frame, fg="white" ) #confi del texto
+def login():
+    nombre = entrada.get()
+    pasword = entrada2.get()
+    if nombre == "humberto" and pasword == "yato":
+        correcta()
+
+# Texto
+
+texto = tk.Label(ventana, text="Nombre", font=("Book Antiqua", 12, "bold"), bg=fondo_frame, fg="white")
 texto.pack()
-texto.place(x=45,y=50) #ubicacion del texto
+texto.place(x=50, y=15)
 
-#Frame
+contra = tk.Label(ventana, text="Contraseña", font=("Book Antiqua", 12, "bold"), bg=fondo_frame, fg="white")
+contra.pack()
+contra.place(x=45, y=65)
+
+# Frame
 
 frame.pack()
-frame.config(bg=fondo_frame) #se le da color al frame
-frame.config(width="385", height="160")#y sus medidas 
+frame.config(bg=fondo_frame)
+frame.config(width="385", height="160")
 
-#ENTRADA
+# Entrada
 
-entrada = tk.Entry(ventana,width= 35, relief= "flat", bg= fondo_entrada)
-entrada.place(x=120 ,y=55 ) 
+entrada = tk.Entry(ventana, width=35, relief="flat", bg=fondo_entrada)
+entrada.place(x=140, y=17)
+entrada2 = tk.Entry(ventana, width=35, relief="flat", bg=fondo_entrada)
+entrada2.place(x=140, y=65)
+entrada2.config(show="*")
 
-#BOTONES
+# Botones
 
-boton1 = tk.Button(ventana, text= "Boton 1", width=7, relief="groove", font=("Book Antiqua",12), bg= fond_botonE, cursor= "pirate") 
-boton1.place(x= 150, y=120)
+boton1 = tk.Button(ventana, text="Boton 1", width=7, relief="groove", font=("Book Antiqua", 12), bg=fond_botonE,
+                   cursor="pirate", command=login)
+boton1.place(x=150, y=120)
+boton1.bind("<Return>", (lambda event: login()))
 
-ventana.mainloop() #con esto corremo la ventana
+ventana.mainloop()
